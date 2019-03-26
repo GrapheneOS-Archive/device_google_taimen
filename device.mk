449ad8f8f4bd38c9a88d79b1cccfb609962cd7f2
Daniel Micay <danielmicay@gmail.com>
2019-03-26 00:20:08 -0400

    move to device-specific kernels

--- device.mk
+++ device.mk
@@ -131,3 +131,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_gl_phase_offset_ns=1000000
 
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+    LOCAL_KERNEL := device/google/taimen-kernel/Image.lz4-dtb
+else
+    LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
